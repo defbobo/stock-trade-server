829a4d031a2d9ef94477ac96121cdb430e6b96f8
defbobo <defbobo@gmail.com>
2016-06-02 12:56:28 +0800

    note: import __future__ must occur at the beginning of the file

--- app/trader_api/views.py
+++ app/trader_api/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import request, jsonify, current_app
 from __future__ import division
+from flask import request, jsonify, current_app
 from datetime import datetime
 import json
 from . import apis
