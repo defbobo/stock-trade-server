af2f2f80249c8c06341b97f5af9e9c8341446f7f
defbobo <defbobo@gmail.com>
2016-06-02 10:25:37 +0800

    add Order deal

--- app/trade_app.py
+++ app/trade_app.py
@@ -6,10 +6,20 @@ from flask import current_app
 
 @celery.task
 def handle_order(order_type, order):
-    # order_match = OrderMatch()
-    # if order_type == 'cancel':
-    #     order_match.cancel(order)
-    # else:
-    #     order_match.submit(order)
-    # dealed_order = order_match.deal()
-    current_app.logger.warning(order)
+    id = order['order_id']
+    timestamp = order['submit_time']
+    symbol = order['symbol']
+    type = order['order_type']
+    price = order['price']
+    amount = order['amount']
+
+    new_order = Order(id, timestamp, symbol, type, price, amount)
+    order_match = OrderMatch()
+
+    if order_type == 'cancel':
+        order_match.cancel(new_order)
+    else:
+        order_match.submit(new_order)
+
+    dealed_order = order_match.deal()
+    current_app.logger.warning(dealed_order)
